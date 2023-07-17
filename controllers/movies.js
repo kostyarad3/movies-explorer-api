f5d645c1442a95f102b2dec511ad47495cf65d8b
Konstantin Radchenko <kostya.rad3@yandex.ru>
2023-07-17 16:54:29 +0300

    feat: added new functionality

---
+++ controllers/movies.js
@@ -0,0 +1,73 @@
+const Movie = require('../models/movie');
+const BadRequestError = require('../errors/bad-request-err');
+const ForbiddenError = require('../errors/forbidden-err');
+const NotFoundError = require('../errors/not-found-err');
+
+function getMovies(req, res, next) {
+  Movie.find({})
+    .then((movies) => res.send({ data: movies }))
+    .catch(next);
+}
+
+function createMovie(req, res, next) {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailer,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+  } = req.body;
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailer,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+    owner: req.user._id,
+  })
+    .then((movie) => res.send({ data: movie }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(
+          new BadRequestError(
+            'Переданы некорректные данные при создании фильма.',
+          ),
+        );
+        return;
+      }
+      next(err);
+    });
+}
+
+function deleteMovie(req, res, next) {
+  Movie.findById(req.params.movieId)
+    .orFail(new NotFoundError('Такого фильма не существует'))
+    .then((card) => {
+      if (card.owner.equals(req.user._id)) {
+        Movie.deleteOne(card)
+          .then((deletedMovie) => res.send({ data: deletedMovie }))
+          .catch(next);
+      } else {
+        next(new ForbiddenError('Нет прав на удаление чужого фильма'));
+      }
+    })
+    .catch(next);
+}
+
+module.exports = {
+  getMovies,
+  createMovie,
+  deleteMovie,
+};
---
+++ utils/constants.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-useless-escape
+const LINK_REGEX = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+// const EN_TEXT_REGEX = /^[a-zA-Z]+$\s/;
+// const RU_TEXT_REGEX = /[\wа-я\sё]/;
+const { NODE_ENV } = process.env;
+const { JWT_SECRET } = process.env;
+const { BASE_URL } = process.env;
+
+module.exports = {
+  JWT_SECRET,
+  NODE_ENV,
+  LINK_REGEX,
+  BASE_URL,
+};
